a21805531333f3a19fbe7b038e4e779af9a12cc2
FredChen <38094428+greatFredChen@users.noreply.github.com>
2020-06-15 21:06:30 +0800

    Add AlexNet documentation.

---
+++ VGGNet/VGGNet.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, \
+    Dropout, Reshape, Dense, BatchNormalization, Flatten
+from tensorflow.keras.optimizers import SGD, Nadam, Adam
+
+
+# construct VGGNet
+def model():
+    pass
